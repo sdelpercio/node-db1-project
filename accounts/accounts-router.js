c88728ae15ebacfe7629029751de64e80a3d5a78
Shawn DelPercio <sdelperc@gmail.com>
2020-03-16 16:25:42 -0400

    added endpoints to create, update, delete accounts

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -24,5 +24,45 @@ router.get('/:id', (req, res) => {
 			res.status(500).json({ error: 'Could not retrieve account.', err });
 		});
 });
+router.post('/', (req, res) => {
+	db('accounts')
+		.insert(req.body, 'id') // we add id to inform postgres to send back id
+		.then(ids => {
+			res.status(201).json({ results: ids });
+		})
+		.catch(err => {
+			res.status(500).json({ error: 'Could not post.', err });
+		});
+});
+router.put('/:id', (req, res) => {
+	db('accounts')
+		.where({ id: req.params.id }) // important to filter fist
+		.update(req.body) // include changes in with updating
+		.then(count => {
+			if (count > 0) {
+				res.status(200).json({ message: 'Record updated.' });
+			} else {
+				res.status(404).json({ message: 'account not found.' });
+			}
+		})
+		.catch(err => {
+			res.status(500).json({ error: 'error updating post.', err });
+		});
+});
+router.delete('/:id', (req, res) => {
+	db('accounts')
+		.where({ id: req.params.id }) // important to filter fist
+		.del() // deletes records
+		.then(count => {
+			if (count > 0) {
+				res.status(200).json({ message: 'Record deleted.' });
+			} else {
+				res.status(404).json({ message: 'account not found.' });
+			}
+		})
+		.catch(err => {
+			res.status(500).json({ error: 'error deleting account.', err });
+		});
+});
 
 module.exports = router;
